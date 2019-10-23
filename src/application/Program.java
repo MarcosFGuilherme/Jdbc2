2adce3968e0ae2db3f979413e6b50ed70c7ee83b
Marcos Guilherme <marcosfs.guilherme@gmail.com>
2019-10-22 21:50:35 -0300

    Update - Insert seller with recovery is inserted.

--- src/application/Program.java
+++ src/application/Program.java
@@ -16,11 +16,62 @@ public class Program {
 	public static void main(String[] args) {
 		
 		//showDepartment();
-		showSeller();
+		//insertSeller();
+		insertSeller2();
 		
 	}
 	
-	public static void showSeller() {
+	public static void insertSeller2() {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		Connection conn = null;
+		ResultSet rs = null;
+		PreparedStatement st = null;
+				
+		try {
+			conn = DB.getConnection();
+			
+			String sql = "INSERT INTO seller "
+					+ " (Name, Email, BirthDate, BaseSalary, DepartmentId)"
+					+ " VALUES "
+					+ "( ?,?,?,?,? )";
+								
+			st = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+			
+			st.setString(1, "Carl Purple");
+			st.setString(2, "carl@gmail.com");
+			st.setDate(3, new java.sql.Date(sdf.parse("20/04/1985").getTime()));
+			st.setDouble(4, 3000.0);
+			st.setInt(5,4);
+			
+			int rowAffeccted = st.executeUpdate();
+			
+			if (rowAffeccted > 0 ) {
+				rs =  st.getGeneratedKeys();
+				while (rs.next()) {
+					int id = rs.getInt(1);
+					System.out.println("Done! Id = " + id);
+				}
+			}
+			else {
+				System.out.println("No rown affected.");
+			}
+			
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		catch ( ParseException e) {
+			e.printStackTrace();
+		}
+		finally {
+			DB.closeResultSet(rs);
+			DB.closeStatemet(st);
+			DB.closeConnection();
+		}
+	}
+	
+	
+	public static void insertSeller() {
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		Connection conn = null;
 		PreparedStatement st = null;
